b9f583a0bc42e169f6672e85140920854d3fa165
chuck002 <ljavy86@gmail.com>
2024-05-17 15:41:36 -0300

    Fix localhost

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
