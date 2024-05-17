f4cebf4dae0ae85479d4eecca39d0201bbeee3ec
chuck002 <ljavy86@gmail.com>
2024-05-17 15:40:16 -0300

    Add CORS Policy

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
